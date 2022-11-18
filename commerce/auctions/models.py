880718110a141021537b73d036ec114b50bd4fba
ronchuoeng <jwei_eng@hotmail.com>
2022-11-18 12:27:33 +0800

    active listing true & page done

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -28,6 +28,7 @@ class Listing(models.Model):
     img = models.URLField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sellers")
+    active = models.BooleanField(default=True)
     
     def __str__(self):
         return f"{self.title}"
